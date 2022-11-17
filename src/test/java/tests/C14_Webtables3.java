a475a7c88c0b256a99656ee81f4455e271c12f62
ibrkorkut07 <http://github.com/ibrkorkut07>
2022-11-17 22:59:52 +0100

    excel files

---
+++ src/test/java/tests/C14_Webtables3.java
@@ -0,0 +1,22 @@
+package tests;
+
+public class C14_Webtables3 {
+
+    // login(login() metodun oluşturun ve oturum
+    // https://qa environment.concorthotel.com/ admin/HotelRoomAdmin adresine gidin
+    //      Username : manager   Password : Manager2!
+    // table(table() metodu oluşturun
+    //      Tüm table body’sinin boyutunu(s utun sayisi ) bulun. /tbody
+    //      Table’daki tum body’i ve başlıkları(headers) konsolda yazdırın.
+    // printRows(printRows() metodu oluşturun //tr
+    //      table body’sinde bulunan toplam satir(row) sayısını bulun.
+    //      Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+    //      4.satirdaki(row) elementleri konsolda yazdırın.
+    // printCells(printCells() metodu oluşturun //td
+    //      table body’sinde bulunan toplam hücre(cell) sayısını bulun.
+    //      Table body’sinde bulunan hücreleri(cells) konsolda yazdırın.
+    // printColumns(printColumns() metodu oluşturun
+    //      table body’sinde bulunan toplam sutun(column) sayısını bulun.
+    //      Table body’sinde bulunan sutunlari(column) konsolda yazdırın.
+    //      5.column daki elementleri konsolda yazdırın.
+}
---
+++ src/test/java/tests/C18_ReadExcel3.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class C18_ReadExcel3 {
+
+    // 1.satirdaki 2.hucreye gidelim ve yazdiralim
+    // 1.satirdaki 2.hucreyi bir string degiskene atayalim ve yazdiralim
+    // 2.satir 4.cell’in afganistan’in baskenti oldugunu test edelim
+    // Satir sayisini bulalim
+    // Fiziki olarak kullanilan satir sayisini bulun
+    // Ingilizce Ulke isimleri ve baskentleri bir map olarak kaydedelim
+}
